b1d40fa1250205ae3100a37a1347f5240799407e
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-03-12 13:28:27 +0700

    Labwork 2

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,45 +1,34 @@
 # Create class Person
 class Person:
-    def __init__(self,name: str):
-        self.name = name
-        
-#Create class Lecturer inheritance Person
-class Lecturer(Person):
-    def get_lecturer(self):
-         return self.name
+    def __init__(self,name):
+        self.__name = name
+    def get_person(self):
+        return self.__name
 #Create class Student inheritance Person
 class Student(Person):
-    def __init__(self,name: str ,id: int ,dob:  str):
+    def __init__(self, id,name,dob):
         super().__init__(name)
-        self.id = id
-        self.dob = dob
+        self.__name = name
+        self.__id = id
+        self.__dob = dob
     def get_student(self):
-        return f"ID: {self.id} Name: {self.name} DOB: {self.dob}"
-        
+        return f"ID: {self.__id}  Name: {self.__name}  DOB: {self.__dob}"
+
 #Create class Subject
 class Subject:
-    def __init__(self, subject_name: str,id_subject: int):
-        self.subject_name = subject_name
-        self.id_subject = id_subject
+    def __init__(self,id_subject,name_subject):
+        self.__id_subject = id_subject
+        self.__name_subject = name_subject
     def get_subject(self):
-        return f"ID: {self.id_subject} Name: {self.subject_name}"
-        
+        return f"ID: {self.__id_subject}  Name: {self.__name_subject}"
+
 #Create class Mark
 class Mark:
-    def __init__(self,mark: int):
-        self.mark = mark
+    def __init__(self,mark):
+        self.__mark = mark
     def get_mark(self):
-        return f"{self.mark}"
-        
-# This class Information is useless because I don't khown how to use multiple inheritance
-class Information(Student,Subject,Mark):
-    def __init__(self, name: str, id: int, dob: str,subject_name: str,id_subject: int,mark: int):
-        Student.__init__(self,name, id, dob)
-        Subject.__init__(self,subject_name,id_subject)
-        Mark.__init__(self,mark)
-    def get_information(self):
-        return f"{self.id} {self.name} {self.subject_name} {self.mark}"
-
+        return f"{self.__mark}"
+    
 # Create an array to store value
 students = []
 all_student = []
@@ -54,7 +43,7 @@ for i in range(n):
     id_stu = int(input(f"ID of student {i+1}: "))
     name_stu = input(f"Name of student {i+1}: ")
     dob_stu = input(f"DOB of student {i+1}: ")
-    stu = Student(name_stu,id_stu,dob_stu)
+    stu = Student(id_stu,name_stu,dob_stu)
     students.append(name_stu)
     all_student.append(stu.get_student())
 
@@ -63,7 +52,7 @@ m = int(input("Enter the number of subject: "))
 for i in range(m):
     id_sub = input(f"ID of the subject {i+1}: ")
     name_sub = input(f"Name of the subject {i+1}: ")
-    sub = Subject(name_sub,id_sub)
+    sub = Subject(id_sub,name_sub)
     subjects.append(name_sub)
     all_subject.append(sub.get_subject())
 
@@ -75,16 +64,19 @@ for i in range(len(students)):
         marks.append(mark.get_mark())
 
 # Output list of student
+print(" ")
 print("List of the student: ")
 for i in range(len(all_student)):
     print(all_student[i])
 
 # Output list of subject
+print(" ")
 print("List of the subjects: ")
 for i in range(len(all_subject)):
     print(all_subject[i])
 
 # Output list mark of student
+print(" ")
 for i in range(len(students)):
     print(f"{students[i]}")
     for j in range(len(subjects)):
